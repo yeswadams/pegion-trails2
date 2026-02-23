68d76519d3f32233c5586b012b5ef15830f7d716
yeswadams <yeswaadams73@gmail.com>
2026-02-23 04:18:10 +0300

    Id fix

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "placeholder",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "7qsmfcus",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2024-01-01",
   useCdn: false, // Set to false for real-time updates or localized caching
