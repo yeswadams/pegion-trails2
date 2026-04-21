899397653b89d8e02391d9daf346a9643aa270dd
Adams Yeswa(Bear) <130048211+yeswadams@users.noreply.github.com>
2026-04-21 12:02:18 +0300

    Fix typo in isServicesPage condition

--- src/_components/shared/navbar.tsx
+++ src/_components/shared/navbar.tsx
@@ -166,7 +166,7 @@ const Navbar = () => {
   const isBlogPage =
     pathname?.includes("/blog") || pathname?.includes("/resources");
   const isPricingPage = pathname?.includes("./pricing");
-  const isServicesPage = pathname?.incldes("./services");
+  const isServicesPage = pathname?.includes("./services");
   const [isScrolled, setIsScrolled] = useState(false);
   const [hoveredNavItem, setHoveredNavItem] = useState<string | null>(null);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
