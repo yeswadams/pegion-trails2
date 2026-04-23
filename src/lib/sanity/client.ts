d308730d971c8b5d20b7d332c8e1ff84ab980c60
yeswadams <yeswaadams73@gmail.com>
2026-04-23 16:12:34 +0300

    separated the website and the studio

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -2,8 +2,8 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "7qsmfcus",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-01-01",
   useCdn: true, // Set to true for edge caching and better reliability on the client side
 });
--- studio-pegiontrails-blog/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import { author } from './author'
-import { postType } from './postType'
-import { category } from './category'
-import { blockContent } from './blockContent'
-export const schemaTypes = [author, postType, category, blockContent]
